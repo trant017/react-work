e5bf8e66c21ee7ba5dc0dca936160a9199e924c3
trant017 <trant017@gmail.com>
2018-10-28 11:06:05 -0400

    Updates

---
+++ precache-manifest.ea7a42fc77126f3294ab27ac78db2a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-work/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e3068395e29b3814415",
+    "url": "/react-work/static/js/runtime~main.3e306839.js"
+  },
+  {
+    "revision": "d3805c83a06109b8f3ae",
+    "url": "/react-work/static/js/main.d3805c83.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-work/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "d3805c83a06109b8f3ae",
+    "url": "/react-work/static/css/main.5a8d40ee.chunk.css"
+  },
+  {
+    "revision": "69d194d6ff6fd0a58b50c273c49fff62",
+    "url": "/react-work/index.html"
+  }
+];
